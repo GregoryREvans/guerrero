405ede5f5491ea52049ccc3e0a5f307c347b6a5f
Gregory Evans <evansdsg2@MacBook-Pro.local>
2018-09-19 21:11:25 -0400

    Still Fixing Attachments.

--- Test/subtest/AttachmentHandler.py
+++ Test/subtest/AttachmentHandler.py
@@ -23,9 +23,10 @@ class AttachmentHandler:
         runs = abjad.select(selections).runs()
         ties = abjad.select(selections).logical_ties()
         for run in runs:
-            abjad.attach(abjad.Dynamic(self.starting_dynamic), run[0])
-            abjad.attach(abjad.DynamicTrend(self.trend), run[0])
-            abjad.attach(abjad.Dynamic(self.ending_dynamic), run[-1])
+            leaves = abjad.select(run).leaves()
+            abjad.attach(abjad.Dynamic(self.starting_dynamic), leaves[0])
+            abjad.attach(abjad.DynamicTrend(self.trend), leaves[0])
+            abjad.attach(abjad.Dynamic(self.ending_dynamic), leaves[-1])
         for tie in ties:
             abjad.attach(self.articulation)
         return selections
