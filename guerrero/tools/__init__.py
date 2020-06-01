64372600bb3009c4cec13a19d533b960e3e5a582
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-06-01 12:00:29 -0400

    Flake8.

--- guerrero/tools/__init__.py
+++ guerrero/tools/__init__.py
@@ -1,3 +1,9 @@
 from .AttachmentHandler import AttachmentHandler
 from .MusicMaker import MusicMaker
 from .TrillHandler import TrillHandler
+
+__all__ = [
+    "AttachmentHandler",
+    "MusicMaker",
+    "TrillHandler",
+]
